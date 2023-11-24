8a3768b12c3a9f358cf1bc659a33e46c1b07220b
Carlos Ortega <carlos.ortega@coppel.com>
2023-11-24 09:25:08 -0700

    Cleanup and basic dependencies

---
+++ src/navigation/RouterConfig.tsx
@@ -0,0 +1,9 @@
+import {Routes, Route} from 'react-router-dom';
+
+export const RouterConfig = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<p>Template works</p>}/>
+    </Routes>
+  )
+}
